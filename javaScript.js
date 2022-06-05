cf1a2e36c87f107f032a497a599b1caea37bb805
davidriver <dav-rc@hotmail.com>
2022-06-05 09:47:07 -0500

    Add functional color picker and clean code

--- javaScript.js
+++ javaScript.js
@@ -49,9 +49,31 @@ function squaresInteractive(){
     }))*/
 }
 
+function toggleRBtn(){
+    if (randomColor){
+        randomBtn.style.backgroundColor = '#66FCF1';
+        randomBtn.style.color = '#1F2833';
+    } else {
+        randomBtn.style.backgroundColor = '#1F2833';
+        randomBtn.style.color = '#C5C6C7';
+    }
+}
+
+function toggleEBtn(){
+    if (eraserMode){
+        eraserBtn.style.backgroundColor = '#66FCF1';
+        eraserBtn.style.color = '#1F2833';
+    } else {
+        eraserBtn.style.backgroundColor = '#1F2833';
+        eraserBtn.style.color = '#C5C6C7';
+    }
+}
+
+//Create grid container div element and set its initial width
 containerDiv = document.querySelector('#container');
 let gridWidth = 16;
 
+//Implement slider for changing grid size
 const pixelsPara = document.querySelector('#gridSize');
 pixelsPara.textContent = `Grid size: ${gridWidth}px`;
 const slider = document.querySelector('#slider');
@@ -63,19 +85,24 @@ slider.addEventListener('change', function(){
     squaresInteractive();
 })
 
-currentColor = 'black';
+//Create initial drawing color, and implement rainbow mode button
+currentColor = 'blue';
 randomColor = false;
 randomBtn = document.querySelector('#randomBtn');
 randomBtn.addEventListener('click', function(){
     randomColor = !randomColor;
+    toggleRBtn();
 })
 
+//Implement eraser mode button
 eraserMode = false;
 eraserBtn = document.querySelector('#eraserBtn');
 eraserBtn.addEventListener('click', function(){
     eraserMode = !eraserMode;
+    toggleEBtn();
 })
 
+//Implement clear grid button
 clearBtn = document.querySelector('#clearBtn');
 clearBtn.addEventListener('click', function(){
     containerDiv.innerHTML = '';
@@ -83,6 +110,7 @@ clearBtn.addEventListener('click', function(){
     squaresInteractive();
 })
 
+//Implement mouseDown variable to check if mouse is clicking
 var mouseDown = 0;
 document.body.onmousedown = function() { 
   ++mouseDown;
@@ -91,5 +119,6 @@ document.body.onmouseup = function() {
   --mouseDown;
 }
 
+//Initiate paintable grid
 createGrid();
 squaresInteractive();
